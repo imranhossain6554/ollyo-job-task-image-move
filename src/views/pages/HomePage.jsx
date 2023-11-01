8609db94ec0817fd02fb05cb905f9765a4d787ea
imranhosain6554 <imran122.office@gmail.com>
2023-11-01 16:25:59 +0600

    fix check

--- src/views/pages/HomePage.jsx
+++ src/views/pages/HomePage.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import images1 from "../../assets/images/image-1.webp";
-import images2 from "../../assets/images/image-2.webp";
+
 const HomePage = () => {
   const [dataImage, setDataImage] = useState([]);
+  const [selectedImages, setSelectedImages] = useState({});
+
   const getData = () => {
     fetch("../../../imageData.json", {
       headers: {
@@ -11,55 +12,48 @@ const HomePage = () => {
       },
     })
       .then(function (response) {
-        console.log(response);
         return response.json();
       })
       .then(function (myJson) {
         setDataImage(myJson);
       });
   };
+
   useEffect(() => {
     getData();
   }, []);
-  console.log("hhh", dataImage);
+
+  const handleImageSelect = (imageSrc) => {
+    setSelectedImages((prevSelectedImages) => ({
+      ...prevSelectedImages,
+      [imageSrc]: !prevSelectedImages[imageSrc],
+    }));
+  };
+
   return (
     <div className="lg:container lg:mx-auto mt-8 mb-10 grid grid-cols-5 gap-4">
-      <div className="col-span-2 row-span-2 w-full border-solid border-2 border-gray-200">
-        <img src={images1} alt="images" />
-      </div>
-      <div className="w-full  border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full  border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full  border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
-      <div className="w-full border-solid border-2 border-gray-200">
-        <img src={images2} alt="images" />
-      </div>
+      {dataImage.map((imageData, index) => (
+        <div
+          key={index}
+          className={`relative w-full border-solid border-2 border-gray-200 ${
+            index === 0 ? "col-span-2 row-span-2" : ""
+          }`}
+        >
+          <img src={imageData.image} alt={`Image ${index}`} />
+          <label
+            className="absolute top-0 left-0 mt-2 ml-2 bg-transparent"
+            style={{ padding: "4px" }}
+          >
+            <input
+              type="checkbox"
+              className="mr-2 bg-transparent"
+              checked={selectedImages[imageData.image] || false}
+              onChange={() => handleImageSelect(imageData.image)}
+            />
+            Select
+          </label>
+        </div>
+      ))}
     </div>
   );
 };
