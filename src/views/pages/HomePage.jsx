d821443b8888bec3c8d67ab61c14cf33f8ce9b46
imranhosain6554 <imran122.office@gmail.com>
2023-11-01 18:04:40 +0600

    almost fix

--- src/views/pages/HomePage.jsx
+++ src/views/pages/HomePage.jsx
@@ -35,7 +35,7 @@ const HomePage = () => {
       <div className="flex justify-between mt-6">
         <p className="ml-6 mt-6">3 files selected</p>
         <button className="text-[red] font-bold mr-6 mt-6" type="button">
-          Delete FIles
+          Delete Files
         </button>
       </div>
       <div className="h-1 w-full border-b-[1px] border-black mb-12 "></div>
@@ -47,21 +47,24 @@ const HomePage = () => {
             className={`relative overflow-hidden w-full border-solid border-2 rounded-md border-gray-200 ${
               index === 0 ? "col-span-2 row-span-2 " : ""
             }`}
+            onMouseEnter={() => handleImageSelect(imageData.image)}
+            onMouseLeave={() => handleImageSelect(imageData.image)}
           >
-            <div className="w-96 h-96 absolute bg-[#9c9898] top-0 opacity-0  hover:opacity-50 max-w-xs transition duration-500 ease-in-out hover:scale-100"></div>
+            <div className="w-96 h-96 absolute bg-[#9c9898] top-0 opacity-0 hover:opacity-50 max-w-xs transition duration-500 ease-in-out hover:scale-100"></div>
             <img src={imageData.image} alt={`Image ${index}`} />
-            <label
-              className="absolute top-0 left-0 mt-2 ml-2 bg-transparent"
-              style={{ padding: "4px" }}
-            >
-              <input
-                type="checkbox"
-                className="mr-2 bg-transparent"
-                checked={selectedImages[imageData.image] || false}
-                onChange={() => handleImageSelect(imageData.image)}
-              />
-              Select
-            </label>
+            {selectedImages[imageData.image] && (
+              <label
+                className="absolute top-0 left-0 mt-2 ml-2 bg-transparent"
+                style={{ padding: "4px" }}
+              >
+                <input
+                  type="checkbox"
+                  className="mr-2 bg-transparent"
+                  onChange={() => handleImageSelect(imageData.image)}
+                />
+                Select
+              </label>
+            )}
           </div>
         ))}
       </div>
